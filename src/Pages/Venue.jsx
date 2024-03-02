da95fc3e844758a2610f0f8514a6c121dd79687a
eyogesh007 <81839475+eyogesh007@users.noreply.github.com>
2024-03-02 14:44:09 +0530

    Add files via upload

--- src/Pages/Venue.jsx
+++ src/Pages/Venue.jsx
@@ -1,49 +1,51 @@
 import React from 'react';
-import { Container, Typography, Box, Grid } from '@mui/material';
-import CustomBreadcrumbs from "../Components/BreadCrums";
+import { styled } from '@mui/system';
 
-const  breadcrumbs= [
-    {name : "Home", href: "/"},
-    {name : "Venue", href: "/venue"}
-]
+const StyledConferenceAnnouncement = styled('div')({
+  maxWidth: '85%',
+  margin: 'auto',
+  padding: 20,
+  background: 'white',
+  borderRadius: 12,
+  boxShadow: '8px 8px 8px 8px rgba(0, 0, 0, 0.1)',
+  textAlign: 'center',
+});
 
-function Venue() {
-    return (
-        <React.Fragment>
+const Heading = styled('h2')({
+  color: '#333',
+  marginBottom: 20,
+});
 
-            <CustomBreadcrumbs breadcrumbsInfo={breadcrumbs}/>
-        <Container>
-            <Box mt={5} mb={5}>
-                <Grid container spacing={3}>
-                    {/* Left side - Address details */}
-                    <Grid item xs={12} sm={6}>
-                        <Typography variant="h6" mb={2} style={{ fontFamily: 'cursive', color: '#333' }}>
-                            Corporate Office Address
-                        </Typography>
-                        <Typography variant="body1" mb={1} style={{ fontFamily: 'Arial, sans-serif', color: '#555' }}>
-                            #1-2-288/23/1, Domalguda, Hyderabad - 500 029, Telangana, India
-                        </Typography>
-                        <Typography variant="body1" mb={1} style={{ fontFamily: 'Arial, sans-serif', color: '#555' }}>
-                            Email: info@sreenidhi.edu.in
-                        </Typography>
-                        <Typography variant="body1" style={{ fontFamily: 'Arial, sans-serif', color: '#555' }}>
-                            Phone: +91 40 27640394 / 2764 0394
-                        </Typography>
-                    </Grid>
+const Paragraph = styled('p')({
+  color: '#555',
+  lineHeight: 1.6,
+});
+const App = () => (
+    <>   <br></br>
+    <br></br> <StyledConferenceAnnouncement>
+    <Heading>We are thrilled to announce the venue of our International Conference on
+Recent trends in Electrical and Computer Applications ICRECA-2024!</Heading>
+    <Paragraph>
+      The Conference will be held at SREENIDHI INSTITUTE OF SCIENCE & TECHNOLOGY, Yamnampet, Ghatkesar, Hyderabad, Telangana 501301
+    </Paragraph>
+    <Paragraph>
+      SNIST is a premier educational institution known for its commitment to excellence in science and technology education.
+    </Paragraph>
+    <Paragraph>
+      We look forward to welcoming you to this exciting conference at SNIST!
+    </Paragraph>
+  </StyledConferenceAnnouncement>
+  <br></br>
+  <br></br>
+  <iframe
+  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3806.0794606213703!2d78.66420387526483!3d17.455910500827237!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bcb7612c2c68369%3A0x9ea4b426b7e3699!2sSreenidhi%20Institute%20of%20Science%20%26%20Technology%20-%20SNIST!5e0!3m2!1sen!2sin!4v1706959827589!5m2!1sen!2sin"
+  width="85%"
+  height="450"
+  style={{ border:'0', margin: 'auto', display: 'block'  }}
+  allowfullscreen=""
+  loading="lazy"
+  referrerpolicy="no-referrer-when-downgrade"
+></iframe></>
+);
 
-                    {/* Right side - Image */}
-                    <Grid item xs={12} sm={6} container justifyContent="center" alignItems="center">
-                        <img
-                            src="https://media.getmyuni.com/azure/college-image/big/sreenidhi-institute-of-science-and-technology-snist-hyderabad.jpg"
-                            alt="Venue"
-                            style={{ width: '100%', borderRadius: '8px', boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)' }}
-                        />
-                    </Grid>
-                </Grid>
-            </Box>
-        </Container>
-        </React.Fragment>
-    );
-}
-
-export default Venue;
+export default App;
---
+++ src/Pages/committee.jsx
@@ -0,0 +1,148 @@
+import React from 'react';
+import { Container, Typography, Box, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper } from "@mui/material";
+
+const CommitteeTable = ({ section, members }) => (
+    <Box my={4} >
+        <TableContainer component={Paper} sx={{ background: '#e6f7ff', width: '100%' }}>
+            <Table>
+                <TableHead>
+                    <TableRow>
+                        <TableCell>{section}</TableCell>
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    {members.map((member, index) => (
+                        <TableRow key={index}>
+                            <TableCell>{member}</TableCell>
+                        </TableRow>
+                    ))}
+                </TableBody>
+            </Table>
+        </TableContainer>
+    </Box>
+);
+
+const Committee = () => {
+    const committees = [
+        {
+            section: "Chief Patrons",
+            members: [
+                "Dr. K.T. Mahee, Chairman, Sreenidhi Educational Group",
+                "Mr. K. Abhijit Rao, CEO"
+            ]
+        },
+        {
+            section: "Patrons",
+            members: [
+                "Dr. C.V. Tomy, Executive Director",
+                "Dr. T. Ch. Siva Reddy, Principal"
+            ]
+        },
+        {
+            section: "General Chair",
+            members: [
+                "Dr. J. Upendar, Officer, University College of Engineering, Osmania University, Hyderabad, Telangana 500007, dr.8500003210@gmail.com, 8500003210.",
+                "Dr.K.Sateesh Kumar, Assistant Professor, Dept of EEE, NIT Tirucharappalli-620015, sateesh@nitt.edu,9441274524"
+            ]
+        },
+        {
+            section: "Conference Chair",
+            members: [
+                "Dr T Vinay Kumar, Assistant Professor, Department of electrical engineering, NIT WARANAGAL.",
+                "Dr. Kiran Kumar Nallamekala, Assistant Professor, SRM University Andhra Pradesh, kirankumar.n@srmap.edu.in, 9441276970"
+            ]
+        },
+        {
+            section: "Conveners",
+            members: [
+                "Dr. Bhargava, Head of EEE Department, SNIST, Hyderabad",
+                "Dr. Aruna Varanasi, Head of CSE Department, SNIST, Hyderabad"
+            ]
+        },
+        {
+            section: "Co-Conveners",
+            members: [
+                "Dr. T. Mamatha, Associate Professor, CSE Department, SNIST, Hyderabad",
+                "Dr. Koganti Srilakshmi, Assistant Professor, EEE Department, SNIST, Hyderabad",
+                "Mrs. G. Sravanthi, Assistant Professor, EEE Department, SNIST, Hyderabad",
+                "Dr. P. Sarath Kumar, Associate Professor, EEE Department, SNIST, Hyderabad"
+            ]
+        },
+        {
+            section: "Advisory Committee",
+            members: [
+                "Dr. K. Sumanth, Professor, EEE Department",
+                "Dr. Pratap shekar puhan, Proff, EEE Department",
+                "Dr. K. Ragavendher Reddy Karsani, Asso. Proff, EEE Dept",
+                "Dr. G. Ramesh, Asso. Proff, EEE Dept",
+                "Dr. K. Kannan, Asso. Proff, EEE Dept",
+                "Dr. V. Ranjith Babu, Asso. Proff, EEE Dept",
+                "Dr. M.T.L.Gayatri, Asso. Proff, EEE Dept",
+                "Dr. T. Abhiram, Assistant Professor, EEE Dept",
+                "Dr. K. Husaain, Assistant Professor, EEE Dept",
+                "Dr. K. K. C. Deekshit, Assistant Professor, EEE Dept",
+                "Dr. Sunil Bhutada, Head of IT Department",
+                "Dr. T. V. Rajnikanth, Head of AI & ML",
+                "Dr.T.Venkata Narayana Rao, Head of Internet of Things",
+                "Dr. K. Shirisha, Head of Cyber Security",
+                "Dr. Md. Jaffer Sadiq, Head of Data Science",
+                "Dr Anup Kumar, Professor, CSE Department",
+                "Dr.Sreedhar Bhukya, Professor, CSE Department",
+                "Dr. G. Padmaja, Professor, CSE Department",
+                "Dr. Niranjan, Professor, CSe Dept",
+                "Dr.H.Balaji, Professor, CSE Dept",
+                "Dr.Dheeraj Sundaragiri, Assoc.Prof, CSEDept",
+                "Dr. M. Ram Chandra, Assoc. Prof, CSE Dept",
+                "Dr.V. Sowmya Devi, Assoc.Prof, CSE Dept."
+            ]
+        },
+        {
+            section: "Organizing Committee",
+            members: [
+                "Dr. V. Ramprasad, Assistant Professor, EEE Department, SNIST, Hyderabad",
+                "Dr. Raghavendra Reddy Karasani, Associate Professor, EEE Department, SNIST, Hyderabad",
+                "Mr. Ch.V. Seshagiri Rao, Assistant Professor, EEE Department, SNIST, Hyderabad",
+                "Sri. K.V.V.P.Chari, Assistant Professor, EEE Department, SNIST, Hyderabad",
+                "E Madhukar, Assistant Professor, CSE Department, SNIST, Hyderabad",
+                "D Ram Babu, Assistant Professor, CSE Department, SNIST, Hyderabad",
+                "Devarapu Sreenivasa Rao, Assistant Professor, CSE Department, SNIST, Hyderabad",
+                "P Durga Prasad, Assistant Professor, CSE Department, SNIST, Hyderabad",
+                "Meeravali Shaik, Assistant Professor, CSE Department, SNIST, Hyderabad",
+                "G Venkanna, Assistant Professor, CSE Department, SNIST, Hyderabad",
+                "B Nikhila, Assistant Professor, CSE Department, SNIST, Hyderabad",
+                "K Spandana, Assistant Professor, CSE Department, SNIST, Hyderabad",
+                "G Yogesh, Assistant Professor, CSE Department, SNIST, Hyderabad",
+                "P Lavanya, Assistant Professor, CSE Department, SNIST, Hyderabad",
+                "B Pavani, Assistant Professor, CSE Department, SNIST, Hyderabad"
+            ]
+        },
+        {
+            section: "Technical Advisory Committee",
+            members: [
+                "Dr. P. Aravindha Babu, Professor, EEE Department, Annamalai University",
+                "Dr. P. Ravi Babu, Professor, EEE Department, SNIST, Hyderabad",
+                "Dr.S.Ravichndran, Professor, EEE Department, SNIST, Hyderabad",
+                "Dr. Alapati RamaDevi, Professor, EEE Department, VRSEC",
+                "Dr. Praveen Kumar Balachandran, Asso. Proff, EEE Dept, VCEH",
+                "Dr. Sreedhar Bhukya, Professor, CSE Dept, SNIST",
+                "Dr. K. C. Sredhar, Assoc.Professor, CSE Dept, SNIST"
+            ]
+        },
+    ];
+
+    return (
+        <React.Fragment>
+            <Container maxWidth="lg">
+                <Box width="100%" sx={{ textAlign: "center" }} paddingTop={3}>
+                    <Typography variant="h6">Details of the Committee:</Typography>
+                </Box>
+
+                {committees.map((committee, index) => (
+                    <CommitteeTable key={index} section={committee.section} members={committee.members} />
+                ))}
+            </Container>
+        </React.Fragment>
+    );
+}
+
+export default Committee;
